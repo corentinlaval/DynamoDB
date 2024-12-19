fad87eaa1f8f24e3fa95ec1ab328dc7c280e0224
corentinlaval <144536099+corentinlaval@users.noreply.github.com>
2024-12-19 11:58:00 +0100

    Revert "Front"

--- christmas-app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { GiftManagementComponent } from './gift-management/gift-management.component';
-import { SantaDashboardComponent } from './santa-dashboard/santa-dashboard.component';
-
-export const routes: Routes = [
-  { path: 'child', component: GiftManagementComponent }, // Route pour la page des enfants
-  { path: 'santa', component: SantaDashboardComponent }, // Route pour le tableau de bord du Père Noël
-  { path: '', redirectTo: '/child', pathMatch: 'full' }, // Redirection par défaut vers /child
-];
